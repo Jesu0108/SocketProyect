1224f2aa0ada6099c486abae4d3a4a412a13371d
Jesu0108 <jartiguez2@gmail.com>
2021-01-29 19:31:18 +0100

    a

--- SSLSocket/src/view/CamionView.java
+++ SSLSocket/src/view/CamionView.java
@@ -4,9 +4,6 @@ import java.io.DataInputStream;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.LinkedList;
 import java.util.Queue;
 
